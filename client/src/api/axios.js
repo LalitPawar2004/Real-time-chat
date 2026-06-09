aeac1981a1428e14e727a02db70ef42111b163d2
LalitPawar2004 <lalitpawar2004@gmail.com>
2026-06-09 17:53:42 +0530

    Add auth token to axios requests

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,4 +1,20 @@
 import axios from 'axios'
+
 const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
-const api = axios.create({ baseURL, withCredentials: true })
+
+const api = axios.create({
+  baseURL,
+  withCredentials: true
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
 export default api
